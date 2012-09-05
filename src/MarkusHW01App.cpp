a7e8ac601ebd32847772207f06a2286a106ab0b3
Markus Ernst <ernst.sukram@gmail.com>
2012-09-05 14:55:11 -0500

    added comments

--- src/MarkusHW01App.cpp
+++ src/MarkusHW01App.cpp
@@ -18,11 +18,29 @@ class MarkusHW01App : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+	/**
+	*set the three parts of a pixel using just the x and y value of and the color
+	*/
 	void setPixel(uint8_t* ar, int x,int y, Color8u c);
+	/**
+	*draw a rectangle givent he top left corner and its width and hight
+	*/
 	void drawRect(uint8_t* ar, int x,int y, int w, int h, Color8u c);
+	/**
+	*draw a circle given the center and the randius and the color
+	*/
 	void drawCirc(uint8_t* ar, int x,int y, double r, Color8u c);
+	/**
+	*draw a line given the two end points
+	*/
 	void drawLine(uint8_t* ar, int x, int y, int x2, int y2, Color8u c);
+	/**
+	*draw a triangle given three points
+	*/
 	void drawTri(uint8_t* ar, int x, int y, int x2, int y2, int x3, int y3, Color8u c);
+	/**
+	*implement any kernel, in this program a blur is inplemented. All the values in the kernel can be changed though.
+	*/
 	void blur(uint8_t* ar, float a,  float b,  float c,  float d,  float e,  float f,  float g,  float h,  float i);
 
   private:
@@ -32,7 +50,6 @@ class MarkusHW01App : public AppBasic {
 	int iteration;
 	Surface* mySurface_;
 	uint8_t* pixelArray;
-	int width;
 	float a;
 	int blured;
 	int trix1;
@@ -52,11 +69,12 @@ class MarkusHW01App : public AppBasic {
 
 void MarkusHW01App::setup()
 {
+	//variables for phase 0
 	color1 = 1.0;
 	color2 = 1.0;
 	color3 = 1.0;
 	iteration = 0;
-	width = 640;
+	//variables to have the picture move and stop
 	blured = 0;
 	trix1 = 0;
 	trix2 = 0;
@@ -70,13 +88,13 @@ void MarkusHW01App::setup()
 	col1 = 0;
 	col2 = 0;
 	col3 = 0;
-
+	//creating the surface
 	mySurface_ = new Surface(1024,1024,false);
 };
 
 void MarkusHW01App::mouseDown( MouseEvent event )
 {
-	
+	//if the picture is blured un blur it, and if it is not blured then blur it
 	if(blured == 0){
 		blured = 1;
 	}
@@ -87,6 +105,7 @@ void MarkusHW01App::mouseDown( MouseEvent event )
 
 void MarkusHW01App::setPixel(uint8_t* ar, int x,int y, Color8u c)
 {
+	//set all three of the colors individually using the cordinates of the pixel
 	ar[y * 1024 * 3 + 3 * x] = c.r;
 	ar[y * 1024 * 3 + 3 * x + 1] = c.g;
 	ar[y * 1024 * 3 + 3 * x + 2] = c.b;
@@ -95,6 +114,7 @@ void MarkusHW01App::setPixel(uint8_t* ar, int x,int y, Color8u c)
 
 void MarkusHW01App::drawRect(uint8_t* ar, int x,int y, int w, int h, Color8u c)
 {
+	//loop through all of the pixels in the rectangle and change their color
 	for(int i = x; i < x + w; i++){
 		for(int j = y; j < y + h; j++){
 			setPixel(ar, i, j, c);
@@ -105,7 +125,7 @@ void MarkusHW01App::drawRect(uint8_t* ar, int x,int y, int w, int h, Color8u c)
 
 void MarkusHW01App::drawCirc(uint8_t* ar, int x,int y, double r, Color8u c)
 {
-
+	//travers around the outside of the circle using the sin and cos finctions
 	for(double i = 0; i < 360; i = i + .01){
 		setPixel(ar, cos(i) * r + x, sin(i) * r + y, c);
 	};
@@ -113,12 +133,15 @@ void MarkusHW01App::drawCirc(uint8_t* ar, int x,int y, double r, Color8u c)
 };
 
 void MarkusHW01App::drawLine(uint8_t* ar, int x, int y, int x2, int y2, Color8u c){
+	//determine the slope of the line
 	int dx = (x2 - x);
 	int dy = (y2 - y);
 	float m = (1.0 * dy) / (1.0 * dx);
 	int yv;
 	int temp;
+	//if it has a slope between 1 and -1 then use this first part of the algorithm
 	if(m <= 1.0 && m >= -1.0){
+		//make sure the line is oriented correctly
 		if(x > x2){
 			temp = x;
 			x = x2;
@@ -127,11 +150,13 @@ void MarkusHW01App::drawLine(uint8_t* ar, int x, int y, int x2, int y2, Color8u 
 			y = y2;
 			y2 = temp;
 		};
+		//go throught all of the x values and find the y value of that pixel
 		for (int i = x; i < x2; i++){
 			yv = m * i - m * x + y + 0.5;
 			setPixel(ar, i, yv, c);
 		};
 	};
+	//the same as above, but using going through the y values instead of x. This i because the slope is not between 1 and -1.
 	if(!(m <= 1.0 && m >= -1.0)){
 		if(y > y2){
 			temp = y;
@@ -150,6 +175,7 @@ void MarkusHW01App::drawLine(uint8_t* ar, int x, int y, int x2, int y2, Color8u 
 };
 
 void MarkusHW01App::drawTri(uint8_t* ar, int x, int y, int x2, int y2, int x3, int y3, Color8u c){
+	//to draw a triangle one must just draw three lines connecting the points of the triangle
 	drawLine(ar, x, y, x2, y2, c);
 	drawLine(ar, x, y, x3, y3, c);
 	drawLine(ar, x3, y3, x2, y2, c);
@@ -157,8 +183,9 @@ void MarkusHW01App::drawTri(uint8_t* ar, int x, int y, int x2, int y2, int x3, i
 };
 
 void MarkusHW01App::blur(uint8_t* ar , float a,  float b,  float c,  float d,  float e,  float f,  float g,  float h,  float i){
-
+	//creat a array to hold the kernel
 	float kernel [9] = {a,b,c,d,e,f,g,h,i};
+	//travers the pixels colors and apply the kernel
 	for(int i = 0; i < 640; i++){
 		for(int j = 0; j < 480; j++){
 			ar[j * 1024 * 3 + 3 * i] = ar[(j - 1) * 1024 * 3 + 3 * (i - 1)] * kernel[0] + ar[(j - 1) * 1024 * 3 + 3 * (i)] * kernel[1] + ar[(j - 1) * 1024 * 3 + 3 * (i + 1)] * kernel[2] + ar[(j) * 1024 * 3 + 3 * (i - 1)] * kernel[3] + ar[(j) * 1024 * 3 + 3 * (i)] * kernel[4] + ar[(j) * 1024 * 3 + 3 * (i + 1)] * kernel[5] + ar[(j + 1) * 1024 * 3 + 3 * (i - 1)] * kernel[6] + ar[(j + 1) * 1024 * 3 + 3 * (i)] * kernel[7] + ar[(j + 1) * 1024 * 3 + 3 * (i + 1)] * kernel[8];
@@ -175,7 +202,7 @@ void MarkusHW01App::update()
 	time++;
 
 	
-
+	//create the random shifting of the shapes and colors
 	if(blured == 0 && time % 3 == 0){
 		console() << (rand() % 9) << endl;
 		trix1 = trix1 + (rand() % 5 - 2);
@@ -190,17 +217,17 @@ void MarkusHW01App::update()
 		col2 = col2 + (rand() % 41 - 20);
 		col3 = col3 + (rand() % 41 - 20);
 	};
-
+	//set the background color
 	drawRect(pixelArray, 0, 0, 640, 480, Color8u(125 + col1, 125 + col2, 125 + col3));
-
+	//draw a rectangle
 	drawRect(pixelArray, 300 + circx, 300 + triy2, 100 + triy1, 100 + trix1, Color8u(255, 0, 0));
-
+	//draw a circle
 	drawCirc(pixelArray, 320 + circx, 240 + circy, 120, Color8u(255,0,0));
-
+	//draw a line
 	drawLine(pixelArray, 400 + trix3, 200 + trix1, 230 + triy2, 240 + triy3, Color8u(0, 0, 255));
-
+	//draw a triangle
 	drawTri(pixelArray, 30 + trix1, 300 + triy1, 500 + trix2, 40 + triy2, 420 + trix3, 355 + triy3, Color8u(200, 0, 100));
-
+	//determine if the image should be blured
 	if(blured == 1)
 	blur(pixelArray, 1/9.0,  1/9.0,  1/9.0,  1/9.0, 1/9.0,  1/9.0,  1/9.0,  1/9.0,  1/9.0);
 
